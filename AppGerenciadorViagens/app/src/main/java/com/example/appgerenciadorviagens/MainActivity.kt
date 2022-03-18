1ef316c5d2f1e3564b92bfcebca6e008ef97ba3f
Sabrina Camargo Blockwel <63020705+sblockwel@users.noreply.github.com>
2022-03-18 20:52:37 -0300

    Ajustes e adicionado tela principal

--- AppGerenciadorViagens/app/src/main/java/com/example/appgerenciadorviagens/MainActivity.kt
+++ AppGerenciadorViagens/app/src/main/java/com/example/appgerenciadorviagens/MainActivity.kt
@@ -1,24 +1,23 @@
 package com.example.appgerenciadorviagens
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.appgerenciadorviagens.ui.theme.AppGerenciadorViagensTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,24 +37,61 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App() {
-    Login()
+    var userState by remember {
+        mutableStateOf("")
+    }
+    var isLogged by remember {
+        mutableStateOf(false)
+    }
+    if (isLogged) {
+        Welcome(name = userState)
+    } else {
+        Login(userState,
+            onUserChange ={
+                userState = it
+            },
+            onSuccess = {
+            isLogged = true
+        })
+    }
+
 }
 
 @Composable
-fun Login() {
+fun Welcome(name: String) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Bem-Vindo(a), ${name}",
+            style = MaterialTheme.typography.h4
+        )
+    }
+}
+
+@Composable
+fun Login(user: String, onUserChange: (String) -> Unit, onSuccess: () -> Unit) {
     Card(
         elevation = 10.dp,
         modifier = Modifier
             .padding(all = 8.dp)
             .fillMaxWidth()
+
     ) {
         Column(
-            verticalArrangement = Arrangement.SpaceBetween,
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .padding(all = 25.dp)
                 .fillMaxSize()
         ) {
+            var passwordState by remember {
+                mutableStateOf("")
+            }
+
+            val context = LocalContext.current
+
             Image(
                 painter = painterResource(id = R.drawable.travelicon),
                 contentDescription = "Logo",
@@ -63,8 +99,11 @@ fun Login() {
                     .size(100.dp)
                     .padding(start = 8.dp)
             )
+            if (user.isNotBlank()) {
+                Text(text = "Bem-Vindo(a), ${user}", style = MaterialTheme.typography.h6)
+            }
             Column(
-                verticalArrangement = Arrangement.Center,
+                verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
                     .padding(all = 25.dp)
@@ -72,14 +111,14 @@ fun Login() {
             ) {
                 OutlinedTextField(
                     modifier = Modifier.fillMaxWidth(),
-                    value = "",
-                    onValueChange = { },
+                    value = user,
+                    onValueChange = { onUserChange(it) },
                     label = { Text("Usuário") },
                 )
                 OutlinedTextField(
                     modifier = Modifier.fillMaxWidth(),
-                    value = "",
-                    onValueChange = { },
+                    value = passwordState,
+                    onValueChange = { passwordState = it },
                     label = { Text("Senha") },
                 )
                 Row(
@@ -94,9 +133,19 @@ fun Login() {
                         Text(text = "Cadastrar-se")
                     }
                 }
-                Spacer(modifier = Modifier.height(16.dp))
+                Spacer(
+                    modifier = Modifier.height(16.dp)
+                )
                 Button(
-                    onClick = { /*TODO*/ },
+                    onClick = {
+                        if (user.equals("admin") && passwordState.equals("admin")) {
+                            onSuccess()
+                            Toast.makeText(context, "Logado!", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(context, "Login inválido!", Toast.LENGTH_SHORT).show()
+
+                        }
+                    },
                     modifier = Modifier.fillMaxWidth()
                 ) {
                     Text(text = "Entrar")
